107d0d16480f0a01fd7cd3c76d6694c50ce37fe1
Nathaniel Daniel <nathaniel.daniel12@gmail.com>
2024-07-22 14:51:48 -0700

    Add ability to load map

---
+++ rpgmv-tool/src/command/commands2py.rs
@@ -0,0 +1,36 @@
+use anyhow::Context;
+use std::fs::File;
+use std::path::PathBuf;
+
+#[derive(Debug, argh::FromArgs)]
+#[argh(
+    subcommand,
+    name = "commands2py",
+    description = "a tool to \"decompile\" scripts to Python for easier inspection"
+)]
+pub struct Options {
+    #[argh(
+        option,
+        long = "input",
+        short = 'i',
+        description = "the path to the input file"
+    )]
+    input: PathBuf,
+
+    #[argh(
+        option,
+        long = "output",
+        short = 'o',
+        description = "the path to the output file",
+        default = "PathBuf::from(\"out.py\")"
+    )]
+    output: PathBuf,
+}
+
+pub fn exec(options: Options) -> anyhow::Result<()> {
+    let input_str = std::fs::read_to_string(&options.input)
+        .with_context(|| format!("failed to read \"{}\"", options.input.display()))?;
+    let input: rpgmv_types::Map = serde_json::from_str(&input_str)?;
+
+    Ok(())
+}
