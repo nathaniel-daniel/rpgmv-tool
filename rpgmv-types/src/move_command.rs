c289653d937122024a5e0a1dad86af6433f166cc
Nathaniel Daniel <nathaniel.daniel12@gmail.com>
2024-07-22 14:26:32 -0700

    Add MoveCommand Type

--- rpgmv-types/src/move_command.rs
+++ rpgmv-types/src/move_command.rs
@@ -1,4 +1,13 @@
 /// A move command
 #[derive(Debug, serde::Serialize, serde::Deserialize, PartialEq)]
-// #[serde(deny_unknown_fields)]
-pub struct MoveCommand {}
+#[serde(deny_unknown_fields)]
+pub struct MoveCommand {
+    /// ?
+    pub code: u32,
+
+    /// ?
+    pub parameters: Option<Vec<()>>,
+
+    /// ?
+    pub indent: Option<u32>,
+}
