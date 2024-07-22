d0f851868cd50e70c99b1d0ffa33f2a719a5c3d6
Nathaniel Daniel <nathaniel.daniel12@gmail.com>
2024-07-22 14:15:11 -0700

    Add Image type

--- rpgmv-types/src/image_file.rs
+++ rpgmv-types/src/image_file.rs
@@ -1,4 +1,22 @@
 /// An image
 #[derive(Debug, serde::Serialize, serde::Deserialize, PartialEq)]
-// #[serde(deny_unknown_fields)]
-pub struct ImageFile {}
+#[serde(deny_unknown_fields)]
+pub struct ImageFile {
+    /// ?
+    #[serde(rename = "tileId")]
+    pub tile_id: u32,
+
+    /// ?
+    #[serde(rename = "characterName")]
+    pub character_name: String,
+
+    /// ?
+    pub direction: u8,
+
+    /// ?
+    pub pattern: u8,
+
+    /// ?
+    #[serde(rename = "characterIndex")]
+    pub character_index: u8,
+}
