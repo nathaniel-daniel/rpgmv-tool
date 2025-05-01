b291d504e9da60fdde8229abcbc1d556544966d2
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2025-05-01 16:46:38 +0000

    deploy: ddeca73a0d336b64e9a722e02e88b618c636f3e2

---
+++ trait.impl/core/default/trait.Default.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rpgmv_tool",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"rpgmv_tool/command/commands2py/config/struct.Config.html\" title=\"struct rpgmv_tool::command::commands2py::config::Config\">Config</a>"]]],["rpgmz_tool",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"rpgmz_tool/command/commands2py/config/struct.Config.html\" title=\"struct rpgmz_tool::command::commands2py::config::Config\">Config</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[336,337]}
