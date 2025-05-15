a24da624d5c3411952fb8b0124945c0adb17e725
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2025-05-15 00:57:41 +0000

    deploy: ba8351500e71c0c1e2e8eeba3456948edb9a1656

---
+++ trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rpgmvp",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"rpgmvp/enum.Error.html\" title=\"enum rpgmvp::Error\">Error</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[250]}
---
+++ trait.impl/eframe/epi/trait.App.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rpgmv_image_viewer",[["impl App for <a class=\"struct\" href=\"rpgmv_image_viewer/struct.App.html\" title=\"struct rpgmv_image_viewer::App\">App</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[154]}
